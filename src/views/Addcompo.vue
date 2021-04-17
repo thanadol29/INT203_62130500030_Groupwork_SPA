3d1b5981663af78ab188b77c124a87071bd8ccca
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-04-17 21:46:30 +0700

    first commit

---
+++ src/views/Addcompo.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div>
+      <h3>Post</h3>
+      <input type="text" placeholder="topic" v-model="title" />
+      <button @click="submit">submit</button>
+    </div>
+
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Addcompo",
+    data() {
+    return {
+      title: "",
+    };
+    
+  },
+  methods: {
+    submit() {
+          axios.post("http://localhost:3000/prev", {
+              title: this.title
+            })
+            
+        } 
+        }
+      
+    
+  
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Addcompo />
+    <Prev />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Addcompo from './Addcompo.vue'
+import Prev from '@/components/prev.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Addcompo,
+    Prev
+  }
+}
+</script>
